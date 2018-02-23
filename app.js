ac38cd80a82558bbd53e905b235c56c2591527d9
Emre-Kul <emrekul35@gmail.com>
2018-02-23 20:06:14 +0300

    sending menu

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+const axios = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
-const token = "";
-const bot = new TelegramBot(token,{polling : true});
 
-bot.on('message',(msg) => { 
+const TOKEN = "";
+const MENU_API_URL = 'https://ytuyemekhane-api.herokuapp.com/';
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.on('message', (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId,"Dont Send Me message");
+    console.log(msg.chat);
+    sendTodaysMenu(chatId);
 });
 
-const getTodaysMenu = function(){
-
+const sendTodaysMenu = function (chatId) {
+    axios.get(MENU_API_URL).
+        then((res) => {
+            const data = res.data;
+            const dateText = `${data.date.day}/${data.date.month}/${data.date.year}`;
+            let lunchMenu = '';
+            let dinnerMenu = '';
+            data.lunch.forEach((lunchFood) => {
+                lunchMenu += lunchFood + "\n";
+            });
+            data.dinner.forEach( (dinnerFood) => {
+                dinnerMenu += dinnerFood + "\n";
+            });
+            bot.sendMessage(chatId, `${dateText}\n -------LUNCH--------\n${lunchMenu}\n----------DINNER-----------\n${dinnerMenu}`);
+            //console.log(data);
+        }).
+        catch((err) => {
+            console.log(err);
+            bot.sendMessage(chatId, 'Error accured when getting todays menu!');
+        });
 }
