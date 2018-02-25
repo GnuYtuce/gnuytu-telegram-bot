66a2fbda8714fe40ae0dd47e26d06f46a3a94f76
Emre-Kul <emrekul35@gmail.com>
2018-02-25 17:00:22 +0300

    nedb config folder removed

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
     TOKEN: process.env.TOKEN || '',
     MENU_API_URL: 'https://ytuyemekhane-api.herokuapp.com/',
-    NEDB_FILENAME: 'db/app.db',
     MONGO_URL: process.env.MONGO_URL || ''
 }
